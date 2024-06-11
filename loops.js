5e8fdeabd0f4c1a401996756db9c4cce46dd9c87
LauraaTanase <zaraflaura@gmail.com>
2024-06-11 16:27:47 -0500

    adding loops.js ; index.html modified

---
+++ loops.js
@@ -0,0 +1,14 @@
+//Scrie un program care afișează numerele de la 1 la 10 folosind o buclă do-while.
+//
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+//Scrie un program care afișează toate numerele pare între 0 și 20 folosind o buclă for.
+//
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
+}
